2697280333f08ae4d74ade8cd0ac0ac6de5cbac6
Ahtisham Ul Hassan <ahtisham.nbc@gmail.com>
2026-04-23 23:47:52 +0500

    Add functionality to download DialoGPT-medium model
    Download and save the DialoGPT-medium model and tokenizer.

--- download_model.py
+++ download_model.py
@@ -1,1 +1,13 @@
 
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+
+model_name = "microsoft/DialoGPT-medium"
+save_path = "./models/dialogpt-medium"
+
+os.makedirs(save_path, exist_ok=True)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+
+tokenizer.save_pretrained(save_path)
+model.save_pretrained(save_path)
